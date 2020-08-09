f1dd689617c24063bdac02b14e8840fec72aa751
Евгений <evgenij.romanko@neoseo.com.ua>
2020-08-09 19:53:20 +0300

    Updates

---
+++ precache-manifest.35ef4c88a572736822920f2d0695ba44.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3eb0a66746223d07c26575184a02f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db09b5cf6d87163f999",
+    "url": "/static/css/2.ff3b42f1.chunk.css"
+  },
+  {
+    "revision": "7b92dd6fcacd1e30e4f5",
+    "url": "/static/css/3.c5a5ae4b.chunk.css"
+  },
+  {
+    "revision": "c53d1101de944363bd73",
+    "url": "/static/css/main.d7a64bd4.chunk.css"
+  },
+  {
+    "revision": "1db09b5cf6d87163f999",
+    "url": "/static/js/2.e2826872.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2826872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b92dd6fcacd1e30e4f5",
+    "url": "/static/js/3.4a0cdc39.chunk.js"
+  },
+  {
+    "revision": "8350611ed98a5215db7c",
+    "url": "/static/js/4.f37aad0d.chunk.js"
+  },
+  {
+    "revision": "fec965afdaa8c8de6b55",
+    "url": "/static/js/5.a3d3c0d7.chunk.js"
+  },
+  {
+    "revision": "40c1b58562870ba39d5c",
+    "url": "/static/js/6.ad921f43.chunk.js"
+  },
+  {
+    "revision": "c53d1101de944363bd73",
+    "url": "/static/js/main.a3bfdb8c.chunk.js"
+  },
+  {
+    "revision": "733e86b6d06089d012af",
+    "url": "/static/js/runtime-main.7fa84751.js"
+  }
+]);
